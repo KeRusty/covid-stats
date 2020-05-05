620a3f41c08bd86a729fce0d430258e06ca51571
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-05-05 17:20:35 +0530

    Android Font Issues Fixed

--- App.js
+++ App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Root, View, Text } from 'native-base';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Navigator from "./src/Navigation/Navigator";
 
 export default function App() {
-    return (
-        <Navigator />
-    );
+
+    const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+    if (!isLoadingComplete) {
+        return (
+            <AppLoading
+                startAsync={loadResourcesAsync}
+                onError={handleLoadingError}
+                onFinish={() => handleFinishLoading(setLoadingComplete)}
+            />
+        );
+    } else {
+        return (
+            <Navigator />
+        );
+    }
+
+    async function loadResourcesAsync() {
+        await Promise.all([
+            Font.loadAsync({
+                Roboto: require('native-base/Fonts/Roboto.ttf'),
+                Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+            }),
+        ]);
+    }
+
+    function handleLoadingError(error) {
+        console.warn(error);
+    }
+
+    function handleFinishLoading(setLoadingComplete) {
+        setLoadingComplete(true);
+    }
 }
+
+
+
+
+
