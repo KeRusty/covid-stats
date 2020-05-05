741b7c0117f7f2628b1d6037c6dacf1a9de222a0
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-05-05 14:46:35 +0530

    Global Stats view page created

---
+++ src/Views/Global/Global.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+
+import Content from '../../Components/ViewBox/ViewBox';
+
+import styles from './Global-styles'
+
+export default function Local() {
+
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetch('https://hpb.health.gov.lk/api/get-current-statistical')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    //console.log(data.data.update_date_time)
+
+    return (
+        <View style={styles.container}>
+
+            <View style={styles.heading}>
+
+                <Text style={styles.headingMainText}>COVID-19 Situation Report</Text>
+                <Text style={styles.headingSubText}>Last Update</Text>
+                {!loading &&
+                    <Text style={styles.headingTimeText}>{data.data.update_date_time}</Text>
+                }
+
+            </View>
+
+            {!loading &&
+                <ScrollView>
+
+                    <Content
+                        firstBoxTitle={"Total Cases"}
+                        firstBoxImage={"total"}
+                        firstBoxContent={data.data.global_total_cases}
+                        secondBoxTitle={"New Cases"}
+                        secondBoxImage={"new"}
+                        secondBoxContent={data.data.global_new_cases}
+                    />
+
+                    <Content
+                        firstBoxTitle={"Recovered"}
+                        firstBoxImage={"recover"}
+                        firstBoxContent={data.data.global_recovered}
+                        secondBoxTitle={"Deaths"}
+                        secondBoxImage={"death"}
+                        secondBoxContent={data.data.global_deaths}
+                    />
+
+                </ScrollView>
+            }
+
+
+        </View>
+    );
+}
