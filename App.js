3456918fb7c2ac1919a6e09192067e366d97821b
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-05-05 11:44:17 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import Navigator from "./src/Navigation/Navigator";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    return (
+        <Navigator />
+    );
+}
