2728f4ed439fd04c1141b037e6a362fe966ffa58
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-05-05 17:20:28 +0530

    Update Hospitals.js

--- src/Components/Hospitals/Hospitals.js
+++ src/Components/Hospitals/Hospitals.js
@@ -9,19 +9,21 @@ export default function Hospitals(props) {
 
     const hospitals = props.content
 
-    console.log(hospitals)
-
     return (
         <View style={styles.container}>
 
             <View style={styles.box}>
+
                 <Text style={styles.headingText}>{hospitals.hospital.name}</Text>
+
                 <Text style={styles.subHeadingTextSimple}>Locals Admitted: {hospitals.cumulative_local}</Text>
                 <Text style={styles.subHeadingTextSimple}>Foreigners Admitted: {hospitals.cumulative_foreign}</Text>
                 <Text style={styles.subHeadingText}>Total Admissions: {hospitals.cumulative_total}</Text>
+
                 <Text style={styles.subHeadingTextSimple}>Locals in Treatment: {hospitals.treatment_local}</Text>
                 <Text style={styles.subHeadingTextSimple}>Foreigners in Treatment: {hospitals.treatment_foreign}</Text>
                 <Text style={styles.subHeadingText}>Total in Treatment: {hospitals.treatment_total}</Text>
+
             </View>
 
         </View >
