a2a61f22cbac8274b420b9afc0815353ddf7a183
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-05-05 16:48:19 +0530

    Changes to header tabs

--- src/Components/Header/Header-styles.js
+++ src/Components/Header/Header-styles.js
@@ -5,6 +5,23 @@ export default StyleSheet.create({
         backgroundColor: '#6a59f2'
     },
     headerText: {
+        color: '#ffffff',
+    },
+    tabStyle: {
+        backgroundColor: '#6a59f2'
+    },
+    textStyle: {
+        color: '#ffffff',
+        fontWeight: 'bold'
+    },
+    activeTabStyle: {
+        backgroundColor: '#6a59f2',
+        fontWeight: 'bold',
+    },
+    activeTextStyle: {
         color: '#ffffff'
+    },
+    tabBarUnderlineStyle: {
+        backgroundColor: '#eae8fd',
     }
 })
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,7 @@ import { Container, Header, Body, Title, Tabs, Tab, View } from 'native-base';
 
 import Local from '../../Views/Local/Local';
 import Global from '../../Views/Global/Global';
+import Hospital from '../../Views/Hospital/Hospital';
 
 import styles from './Header-styles'
 
@@ -13,22 +14,27 @@ export default function AppHeader() {
             <Header style={styles.header} hasTabs>
 
                 <Body>
-                    <Title style={styles.headerText}>Statistics</Title>
+                    <Title style={styles.headerText}>COVID-19 Statistics Sri Lanka</Title>
                 </Body>
 
             </Header>
 
-            <Tabs>
-                <Tab heading="Local">
+            <Tabs tabBarUnderlineStyle={styles.tabBarUnderlineStyle}>
+
+                <Tab heading="Local" tabStyle={styles.tabStyle} textStyle={styles.textStyle} activeTabStyle={styles.activeTabStyle} activeTextStyle={styles.activeTextStyle}>
                     <Local />
-                </Tab>
-                <Tab heading="Global">
+                </Tab >
+
+                <Tab heading="Global" tabStyle={styles.tabStyle} textStyle={styles.textStyle} activeTabStyle={styles.activeTabStyle} activeTextStyle={styles.activeTextStyle}>
                     <Global />
                 </Tab>
-                <Tab heading="PCR Tests">
-                    <Local />
+
+                <Tab heading="Hospital Info" tabStyle={styles.tabStyle} textStyle={styles.textStyle} activeTabStyle={styles.activeTabStyle} activeTextStyle={styles.activeTextStyle}>
+                    <Hospital />
                 </Tab>
+
             </Tabs>
+
         </Container>
 
     );
