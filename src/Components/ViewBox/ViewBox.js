35c398093d29f0c63cb23dc4eb89b0f1245f58fb
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-05-05 14:58:04 +0530

    Number Formated with thousand seperator

--- src/Components/ViewBox/ViewBox.js
+++ src/Components/ViewBox/ViewBox.js
@@ -18,13 +18,13 @@ export default function ViewBox(props) {
             <View style={styles.box}>
                 <Image style={styles.image} source={props.firstBoxImage === "total" ? TotalImage : props.firstBoxImage === "new" ? NewImage : props.firstBoxImage === "recover" ? RecoverImage : null} />
                 <Text style={props.firstBoxImage === "total" ? styles.totalText : props.firstBoxImage === "new" ? styles.newText : props.firstBoxImage === "recover" ? styles.recoveredText : null}>{props.firstBoxTitle}</Text>
-                <Text style={props.firstBoxImage === "total" ? styles.totalText : props.firstBoxImage === "new" ? styles.newText : props.firstBoxImage === "recover" ? styles.recoveredText : null}>{props.firstBoxContent}</Text>
+                <Text style={props.firstBoxImage === "total" ? styles.totalText : props.firstBoxImage === "new" ? styles.newText : props.firstBoxImage === "recover" ? styles.recoveredText : null}>{props.firstBoxContent.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Text>
             </View>
 
             <View style={styles.box}>
                 <Image style={styles.image} source={props.secondBoxImage === "active" ? ActiveImage : props.secondBoxImage === "quaratine" ? QuarantineImage : props.secondBoxImage === "death" ? DeathImage : props.secondBoxImage === "new" ? NewImage : null} />
                 <Text style={props.secondBoxImage === "active" ? styles.activeText : props.secondBoxImage === "quaratine" ? styles.quarantineText : props.secondBoxImage === "death" ? styles.deathText : props.secondBoxImage === "new" ? styles.newText : null}>{props.secondBoxTitle}</Text>
-                <Text style={props.secondBoxImage === "active" ? styles.activeText : props.secondBoxImage === "quaratine" ? styles.quarantineText : props.secondBoxImage === "death" ? styles.deathText : props.secondBoxImage === "new" ? styles.newText : null}>{props.secondBoxContent}</Text>
+                <Text style={props.secondBoxImage === "active" ? styles.activeText : props.secondBoxImage === "quaratine" ? styles.quarantineText : props.secondBoxImage === "death" ? styles.deathText : props.secondBoxImage === "new" ? styles.newText : null}>{props.secondBoxContent.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Text>
             </View>
 
         </View >
