5d32a094d6426919e9a990016b2d221124556551
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-11-01 17:17:30 +0530

    Sri Lanka only screen added

---
+++ src/Views/SriLanka/SriLanka.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { Text, TouchableOpacity, ScrollView, View } from 'react-native';
+
+import Header from '../../Components/Header/Header';
+
+import styles from './SriLanka-styles'
+
+export default function SriLanka(props) {
+
+    return (
+        <View style={styles.container}>
+
+            <Header />
+
+        </View>
+    );
+}
