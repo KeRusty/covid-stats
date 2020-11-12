f0be3d06dc15fb86418827ef9be526da8e69137c
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-11-12 18:51:41 +0530

    Changes to Automated Tested

--- jest.config.js
+++ jest.config.js
@@ -1,47 +1,7 @@
-
-'use strict';
-
 module.exports = {
-    'transform': {
-        preset: 'jest-expo',
-        transformIgnorePatterns: [
-            'node_modules/(?!((jest-)?react-native|react-clone-referenced-element|expo(nent)?|@unimodules/.*|jest-expo/.*|@expo(nent)?/.*|react-navigation|@react-navigation/.*|sentry-expo|native-base))',
-        ]
-    },
-    'setupFiles': [
-        './jest/setup.js',
-    ],
-    'timers': 'fake',
-    'testRegex': '/__tests__/.*-test\\.js$',
-    'testPathIgnorePatterns': [
-        '/node_modules/',
-        '<rootDir>/template',
-        'Libraries/Renderer',
-        'RNTester/e2e',
-    ],
-    'haste': {
-        'defaultPlatform': 'ios',
-        'platforms': [
-            'ios',
-            'android',
-        ],
-    },
-    'unmockedModulePathPatterns': [
-        'node_modules/react/',
-        'Libraries/Renderer',
-        'promise',
-        'source-map',
-        'fastpath',
-        'denodeify',
-        'fbjs',
-    ],
-    'testEnvironment': 'node',
-    'collectCoverageFrom': [
-        'Libraries/**/*.js',
-    ],
-    'coveragePathIgnorePatterns': [
-        '/__tests__/',
-        '/vendor/',
-        '<rootDir>/Libraries/react-native/',
-    ],
+    verbose: true,
+    preset: "react-native",
+    transformIgnorePatterns: [
+        "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)"
+    ]
 };
